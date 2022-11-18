f4d62a1d320e3af2f5632118809069a773852180
unknown <samdshakilofficial@gmail.com>
2022-11-18 15:38:54 +0500

    jwt token add port 1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -28,6 +29,9 @@ async function run() {
     const bookingsCollection = client
       .db("dectorProtealandddata")
       .collection("bookings");
+    const usersCollection = client
+      .db("dectorProtealandddata")
+      .collection("users");
 
     app.get("/apponment", async (req, res) => {
       // class 5 add date fext---------------------------
@@ -129,6 +133,27 @@ async function run() {
       }
       const result = await bookingsCollection.insertOne(booking);
 
+      res.send(result);
+    });
+    // class 75 add jwt token ------------------------------..>
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+          expiresIn: "1h",
+        });
+        return res.send({ accessToken: token });
+      }
+      res.status(403).send({ accessToken: " " });
+      console.log(user);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await usersCollection.insertOne(user);
       res.send(result);
     });
   } finally {
